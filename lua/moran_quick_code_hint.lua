65db34382e7a8bb4c995bfa5b0201a9d8d15bc80
Elflare <271374942@qq.com>
2024-12-30 20:41:11 +0800

    fix quick code hint bug

--- lua/moran_quick_code_hint.lua
+++ lua/moran_quick_code_hint.lua
@@ -1,58 +1,53 @@
 local Module = {}
 
 function Module.init(env)
-    env.enable_quick_code_hint = env.engine.schema.config:get_bool("moran/enable_quick_code_hint")
-    if env.enable_quick_code_hint then
-        -- The user might have changed it.
-        local dict = env.engine.schema.config:get_string("fixed/dictionary")
-        env.quick_code_hint_reverse = ReverseLookup(dict)
-    else
-        env.quick_code_hint_reverse = nil
-    end
+	env.enable_quick_code_hint = env.engine.schema.config:get_bool("moran/enable_quick_code_hint")
+	if env.enable_quick_code_hint then
+		-- The user might have changed it.
+		local dict = env.engine.schema.config:get_string("fixed/dictionary")
+		env.quick_code_hint_reverse = ReverseLookup(dict)
+	else
+		env.quick_code_hint_reverse = nil
+	end
 
-    env.quick_code_indicator = env.engine.schema.config:get_string("moran/quick_code_indicator") or "⚡"
-    env.enable_aux_hint = env.engine.schema.config:get_bool("moran/enable_aux_hint")
+	env.enable_aux_hint = env.engine.schema.config:get_bool("moran/enable_aux_hint")
 end
 
 function Module.fini(env)
-    env.quick_code_hint_reverse = nil
+	env.quick_code_hint_reverse = nil
 end
 
 function Module.func(translation, env)
-    if (not env.enable_quick_code_hint) or env.quick_code_hint_reverse == nil then
-        for cand in translation:iter() do
-            yield(cand)
-        end
-        return
-    end
+	if (not env.enable_quick_code_hint) or env.quick_code_hint_reverse == nil then
+		for cand in translation:iter() do
+			yield(cand)
+		end
+		return
+	end
 
-    -- Look up if the "geniune" candidate is already in the qc dict
-    local indicator = env.quick_code_indicator
-    if env.enable_aux_hint and indicator == "" then
-        indicator = "⚡"
-    end
-    for cand in translation:iter() do
-        local gcand = cand:get_genuine()
-        local word = gcand.text
-        local all_codes = env.quick_code_hint_reverse:lookup(word)
-        if all_codes then
-            local codes = {}
-            for code in all_codes:gmatch("%S+") do
-                if #code < 4 and code ~= cand.preedit and #cand.preedit > #code and not string.find(cand.preedit, "`") then
-                    table.insert(codes, code)
-                end
-            end
-            if #codes > 0 then
-                -- do not show two indicators
-                if gcand.comment == indicator then
-                    gcand.comment = gcand.comment .. table.concat(codes, " ")
-                else
-                    gcand.comment = gcand.comment .. indicator .. table.concat(codes, " ")
-                end
-            end
-        end
-        yield(cand)
-    end
+	for cand in translation:iter() do
+		local gcand = cand:get_genuine()
+		local word = gcand.text
+		local all_codes = env.quick_code_hint_reverse:lookup(word)
+		if all_codes then
+			local codes = {}
+			for code in all_codes:gmatch("%S+") do
+				if
+					#code < 4
+					and #cand.preedit > #code
+				then
+					table.insert(codes, code)
+				end
+			end
+			if #codes > 0 then
+				-- do not show in reverse lookup mode
+				if gcand.comment ~= table.concat(codes, " ") then
+					gcand.comment = gcand.comment .. table.concat(codes, " ")
+				end
+			end
+		end
+		yield(cand)
+	end
 end
 
 return Module
